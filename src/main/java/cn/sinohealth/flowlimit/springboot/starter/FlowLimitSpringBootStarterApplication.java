7da8c60bf7f1980f1140303fc226d7e3b605cd4a
wenqiaogang <wenqiaogang@sinohealth.cn>
2022-07-26 10:27:53 +0800

    初始化项目

---
+++ src/main/java/cn/sinohealth/flowlimit/springboot/starter/FlowLimitSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package cn.sinohealth.flowlimit.springboot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowLimitSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowLimitSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/sinohealth/flowlimit/springboot/starter/FlowLimitSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sinohealth.flowlimit.springboot.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowLimitSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
